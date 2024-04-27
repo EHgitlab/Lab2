238d270156ac7f9010bdcdc7610ff9650ad567c2
Ernest <ERNESTHO09.24@ichat.sp.edu.sg>
2024-04-27 15:36:07 +0800

    Changes made to code

--- Lab2.py
+++ Lab2.py
@@ -1,6 +1,37 @@
-def main():
-    print("ETO0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-display_main_menu()
-    num_list = get_user_input()
+from statistics import median
+def display_main_menu(listoNume):
+    print("List:", listoNume)
 
-if__name__=="__main__": main()
+def get_user_input(li):
+    listoNum1 = li.split(",")
+    listoNum = [float(i) for i in listoNum1]
+    return listoNum
+
+def calc_average_temperature(listoNume):
+    Sum = sum(listoNume)
+    Num = len(listoNume)
+    average = Sum/Num
+    aver = float(average)
+    return aver
+
+def calc_min_max_temperature(listoNume):
+    Min = min(listoNume)
+    Max = max(listoNume)
+    return Min, Max
+
+def sort_temperature(listoNume):
+    listoNume.sort()
+    return listoNume
+
+def main():
+    listoNume = get_user_input(input())
+    display_main_menu(listoNume)
+    avr = calc_average_temperature(listoNume)
+    print("Average: " + str(avr))
+    temp = calc_min_max_temperature(listoNume)
+    print(f"Minimum and Maximum: {list(temp)}")
+    sort1 = sort_temperature(listoNume)
+    print("Sorted list:", sort1)
+
+if __name__ == "__main__":
+    main()
